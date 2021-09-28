3a3fd11643986353e9c37fc49ce8872ec04fea4e
Francesco Micunco <80624232+FrancescoMicunco@users.noreply.github.com>
2021-09-28 16:15:13 +0200

    extra n. 2

--- function.js
+++ function.js
@@ -2,3 +2,7 @@ let h1 = document.getElementById("firt-title")
 const changeTitle = function() {
     h1.innerText = "HoOkipa Bay"
 }
+
+const changeBackground = function() {
+    document.body.style.backgroundColor = "blue";
+}
